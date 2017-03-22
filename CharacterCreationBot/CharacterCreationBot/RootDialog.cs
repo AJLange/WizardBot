81ec06baf6f6063f7a071b09870b1a52a3f893d6
Katherine Harris <kvm.harris@gmail.com>
2017-03-21 22:02:35 -0700

    Added start async to Root Dialog

--- CharacterCreationBot/CharacterCreationBot/RootDialog.cs
+++ CharacterCreationBot/CharacterCreationBot/RootDialog.cs
@@ -5,11 +5,34 @@ using System.Web;
 using Microsoft.Bot.Builder.Luis;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Luis.Models;
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector;
 
 namespace CharacterCreationBot
 {
     public class RootDialog : IDialog<object>
     {
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
 
+        // Test building Dictionary
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var message = await result;
+
+            // Build dictionary
+            if (RaceDictionary.raceDictionary.Count < 1)
+            {
+                RaceDictionary.BuildRacesFromJSON();
+
+
+            }
+            // Retrieve Objects
+
+            await context.PostAsync(message.Text);
+            context.Wait(MessageReceivedAsync);
+        }
     }
 }
