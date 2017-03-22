853b9476b23c8277864475c2fa3c71cb02d6c803
Katherine Harris <kvm.harris@gmail.com>
2017-03-21 21:39:50 -0700

    Fixed bug in controller after converting HandleSytemMessage to Task

--- CharacterCreationBot/CharacterCreationBot/Controllers/MessagesController.cs
+++ CharacterCreationBot/CharacterCreationBot/Controllers/MessagesController.cs
@@ -70,7 +70,6 @@ namespace CharacterCreationBot
             {
             }
 
-            return null;
         }
     }
 }
--- CharacterCreationBot/CharacterCreationBot/RootDialog.cs
+++ CharacterCreationBot/CharacterCreationBot/RootDialog.cs
@@ -8,16 +8,8 @@ using Microsoft.Bot.Builder.Luis.Models;
 
 namespace CharacterCreationBot
 {
-    public class RootDialog : LuisDialog<object>
+    public class RootDialog : IDialog<object>
     {
-        //public LuisServiceResult BestResultFrom(IEnumerable<LuisResult> results)
-        //{
-        //    var winners = from result in results
-        //                  let resultWinner = BestIntentFrom(result)
-        //                  where resultWinner != null
-        //                  select new LuisServiceResult(result, resultWinner);
-        //    var nonNoneWinner = winners.Where(i => i.BestIntent.Intent != "None").MaxBy(i => i.BestIntent.Score ?? 0d);
-        //    return nonNoneWinner ?? winners.MaxBy(i => i.BestIntent.Score ?? 0d);
-        //}
+
     }
 }
